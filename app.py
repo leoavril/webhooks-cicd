afc9de4eb0e8182194b97106f611fe622502be50
leo <leo.avril1908@gmail.com>
2024-01-16 16:05:04 +0100

    modfified script for webhooks

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ items = []
 
 @app.route('/staging', methods=['POST'])
 def staging():
-    os.system("git pull origin staging")
+    os.system("git pull origin main")
     os.system("pip3 install -r requirements.txt")
     os.system("python -m unittest test-app.py")
     os.system("python test-endtoend-app.py")
